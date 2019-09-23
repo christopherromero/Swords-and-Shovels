dc518171466af4e71303dbf32d14c6f64f5a8d88
Christopher Romero <christopherromero11@gmail.com>
2019-09-23 10:36:17 -0700

    Level 1
    Added: particle effects, lighting, models and animation, cinemamachine camera, audio, and more

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void SceneSwitch()
+    {
+        int sceneNumber = 1;
+        SceneManager.LoadScene(sceneNumber);
+    }
+
+}
